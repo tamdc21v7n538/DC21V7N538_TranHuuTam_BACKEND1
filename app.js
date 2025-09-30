ea434403f0d13659aaa5415b29a48113dbc105a9
Tran Huu Tam <tamdc21v7n538@vlvh.ctu.edu.vn>
2025-09-30 22:20:56 +0700

    Tao controller va routes cho Contact

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const cors = require("cors");
+
+const cors = require(const contactsRouter = require("./app/routes/contact.route");"cors");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book Application." });
---
+++ app/routes/contact.route.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const contacts = require("../controllers/contact.controller");
+
+const contactsRouter = require("./app/routes/contact.route");
+
+const router = express.Router();
+
+router.route("/")
+    .get(contacts.findAll)
+    .post(contacts.create)
+    .delete(contacts.deleteAll);
+
+
+router.route("/favorite")
+    .get(contact.findAllFavorite);
+
+
+router.route("/:id")
+    .get(contacts.findOne)
+    .put(contacts.update)
+    .delete(contact.delete);
+
+
+module.exports = router;
